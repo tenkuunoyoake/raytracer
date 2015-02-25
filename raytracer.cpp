6c74432e782a09423e6905425c6d34a7e5c68733
Elbert <elbertlin168@berkeley.edu>
2015-02-24 21:19:42 -0800

    Add Vector class

--- raytracer.cpp
+++ raytracer.cpp
@@ -17,12 +17,74 @@
 using namespace std;
 
 //****************************************************
-// Classes
+// Classes and Methods
 //****************************************************
+class Vector {                                        // note: vectors are simply [x,y,z]
+  public:                                             // so things like points and colors use Vector
+    float x, y, z;
+    Vector (float, float, float);
+    Vector& operator+=(const Vector& rhs) {
+      x += rhs.x;
+      y += rhs.y;
+      z += rhs.z;
+      return *this;
+    }
+    Vector& operator-=(const Vector& rhs) {
+      x -= rhs.x;
+      y -= rhs.y;
+      z -= rhs.z;
+      return *this;
+    }
+    Vector& operator*=(const float scalar) {
+      x *= scalar;
+      y *= scalar;
+      z *= scalar;
+      return *this;
+    }
+    Vector& operator/=(const float scalar) {
+      x /= scalar;
+      y /= scalar;
+      z /= scalar;
+      return *this;
+    }
+    
+};
+
+Vector::Vector (float a, float b, float c) {
+  x = a;
+  y = b;
+  z = c;
+}
+
+Vector operator+(Vector lhs, const Vector& rhs) {
+  return lhs += rhs;
+}
+
+Vector operator-(Vector lhs, const Vector& rhs) {
+  return lhs -= rhs;
+}
+
+Vector operator*(Vector lhs, const float scalar) {
+  return lhs *= scalar;
+}
+
+Vector operator*(const float scalar, Vector& rhs) {
+  return rhs *= scalar;
+}
+
+Vector operator/(Vector lhs, const float scalar) {
+  return lhs /= scalar;
+}
+
+Vector operator/(const float scalar, Vector& rhs) {
+  return rhs /= scalar;
+}
 
 //****************************************************
 // Global Variables
 //****************************************************
+int image_width;
+int image_height;
 
 int main(int argc, char *argv[]) {
   return 0;
