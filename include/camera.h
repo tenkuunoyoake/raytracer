31d51bf1700ff7b8a625b4a77e851ff5f7f88cd3
Austin Tai <dingaling279@berkeley.edu>
2015-03-11 18:17:04 -0700

    cleaned up camera

--- include/camera.h
+++ include/camera.h
@@ -30,22 +30,8 @@ class Camera {
     Vector origin, uLeft, uRight, lLeft, lRight;
     Matrix transform;
 
-    float distance; 
-
-    float c_left;
-    float c_right;
-    float c_top;
-    float c_bottom;
-
-    Vector u_dir;
-    Vector v_dir;
-    Vector w_dir;
-
     // Methods
-    void initialise_image_plane();
     void do_transform(Matrix c_transform);
-    void compute_viewing_rays(Ray* rays, int rays_size, int i, int j, 
-        int width, int height);
     
     // Constructors
     Camera();
