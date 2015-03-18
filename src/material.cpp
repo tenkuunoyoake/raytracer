52dafa975b7a9b176ddbdf35c9b682e0923c9a9b
Austin Tai <dingaling279@berkeley.edu>
2015-03-17 18:30:23 -0700

    set anti-alias to off when samples = 1

--- src/material.cpp
+++ src/material.cpp
@@ -48,44 +48,6 @@ Vector Material::ambient_c(Vector color) {
 
 }
 
-/*
-
-Vector Material::reflection_v(Vector direction, Vector normal) {
-
-  Vector reflection;    
-  Vector scaled_normal;
-
-  reflection = -1 * direction;
-
-  float normal_scalar = 2 * Vector::dot(direction, normal);
-
-  reflection = reflection + (normal * normal_scalar); 
-  reflection = reflection.normalize();
-
-  return reflection;
-
-}
-
-Vector Material::specular_c(Vector light_color, Vector light_direction,
-    Vector viewer, Vector normal) {
-
-  // C = Ks * I * (r . v) ^ p
-
-  Vector reflection = reflection_v(light_direction, normal);
-
-  float intensity = 0;
-
-  if (Vector::dot(normal, light_direction) >= 0.0f) {
-    intensity = max(0.0f, Vector::dot(viewer, reflection));
-    intensity = pow(intensity, phong_e);
-  }
-
-  return Vector::point_multiply(specular, light_color) * intensity;
-
-}
-
-*/
-
 Vector Material::specular_c(Vector color, Vector light, Vector viewer, 
     Vector normal) {
 
--- src/sampler.cpp
+++ src/sampler.cpp
@@ -20,11 +20,19 @@ void Sampler::get_points(vector<Vector>* points, Camera* camera, int i, int j, i
 
   // Generate x * y random samples 
 
+  float off_x;
+  float off_y;
+
   for (int y = 0; y < samples; y++) {
     for (int x = 0; x < samples; x++) {
 
-      float off_x = (x + (((float) rand()) / RAND_MAX)) / samples;
-      float off_y = (y + (((float) rand()) / RAND_MAX)) / samples; 
+      if (samples == 1) {
+        off_x = x + 0.5;
+        off_y = y + 0.5; 
+      } else {
+        off_x = (x + (((float) rand()) / RAND_MAX)) / samples;
+        off_y = (y + (((float) rand()) / RAND_MAX)) / samples; 
+      }
 
       left = camera->uLeft + (camera->lLeft - camera->uLeft) * (j + off_y) / height;
       right = camera->uRight + (camera->lRight - camera->uRight) * (j + off_y) / height;
